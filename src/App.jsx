a381fcde1acc809c59483f2e996e1d3f3ed8eb23
Chinmay <sahoochinmay54@gmail.com>
2020-07-25 16:17:33 +0530

    A basic increment and decrement page completed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [num, setnum] = useState(0);
+  const inc = () => {
+    setnum(num + 1);
+  };
+  const dec = () => {
+    if (num > 0) {
+      setnum(num - 1);
+    } else {
+      alert("You reach Zero position.");
+    }
+  };
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{num}</h1>
+          <div className="btn_div">
+            <button onClick={inc}>Increm</button>
+            <button onClick={dec}>Decrem</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
